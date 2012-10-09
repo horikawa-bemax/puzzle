f2b9dda1c93a2575d58f8395984f3ae4fc25e02f
Masaaki Horikawa <horikawa@bemax.jp>
2012-10-09 11:19:00 +0900

    タイトルを[15 Puzzle]に変更

--- src/bemax/puzzle/Puzzle.java
+++ src/bemax/puzzle/Puzzle.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -20,40 +21,40 @@ public class Puzzle extends Thread implements SurfaceHolder.Callback, OnTouchLis
 	private Bitmap picture;
 	private Panel[] panels;
 	private int startX, startY, oldX, oldY, column, row, num;
-	
+
 	public Puzzle(SurfaceView v){
 		puzView = v;
 		holder = puzView.getHolder();
 		holder.addCallback(this);
 		puzView.setOnTouchListener(this);
-		
+
 		picture = BitmapFactory.decodeResource(puzView.getResources(), R.drawable.picture);
 		picRect = new Rect(0, 0, picture.getWidth(), picture.getHeight());
 	}
-	
+
 	public void run(){
 		Paint frame_paint = new Paint();
 		frame_paint.setColor(Color.BLACK);
 		frame_paint.setStyle(Paint.Style.STROKE);
-		
+
 		panels = new Panel[16];
 		for(int i=0; i<16; i++){
 			panels[i] = new Panel(i, picture);
 		}
-		
+
 		Rect r = new Rect(0, 0, 120, 120);
-		
+
 		while(loop){
 			Canvas canvas = holder.lockCanvas();
 			canvas.drawColor(Color.WHITE);
-			
+
 			for(int i=0; i<panels.length; i++){
 				if(panels[i].getNum()==15) continue;
 				canvas.drawBitmap(panels[i].getImage(), r, panels[i].getRect(), null);
 			}
-			
+
 			canvas.drawRect(picRect, frame_paint);
-			
+
 			holder.unlockCanvasAndPost(canvas);
 		}
 	}
@@ -62,6 +63,7 @@ public class Puzzle extends Thread implements SurfaceHolder.Callback, OnTouchLis
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 		// TODO 変更されたviewの幅と高さに合わせる
 		viewRect = new Rect(0, 0, width, height);
+		Log.d("create_view",""+width+":"+height);
 	}
 
 	@Override
@@ -87,7 +89,7 @@ public class Puzzle extends Thread implements SurfaceHolder.Callback, OnTouchLis
 		if(!picRect.contains(x, y)){
 			return false;
 		}
-		
+
 		switch(event.getAction()){
 		case MotionEvent.ACTION_DOWN:
 			startX = oldX = x;
