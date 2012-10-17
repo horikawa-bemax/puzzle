47ed3864674e0a0efcf8ef3b14e150d1aa1838e9
Masaaki Horikawa <horikawa@bemax.jp>
2012-10-17 20:55:27 +0900

    コメントを詳細に入力

--- src/bemax/puzzle/MainActivity.java
+++ src/bemax/puzzle/MainActivity.java
@@ -3,35 +3,65 @@ package bemax.puzzle;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity implements OnClickListener{
+/**
+ * メインアクティビティ
+ * @author horikawa
+ */
+public class MainActivity extends Activity implements OnClickListener, OnTouchListener{
 	private SurfaceView puzView;
+	private ImageView bunner;
 	private Button button;
 	private Puzzle puzzle;
 
-    @Override
+	/**
+	 * このアクティビティが作成されたとき
+	 */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /* ここから入力 */
+
+        /* パズルの初期化 */
         puzView = (SurfaceView)findViewById(R.id.puzzle_view);
-        button = (Button)findViewById(R.id.reset_button);
         puzzle = new Puzzle(puzView);
+
+        /* ボタンの初期化 */
+        button = (Button)findViewById(R.id.reset_button);
         button.setOnClickListener(this);
+
+        /* バナーの初期化 */
+        bunner = (ImageView)findViewById(R.id.bunner);
+        bunner.setOnTouchListener(this);
     }
 
-    @Override
+    /**
+     * オプションメニューが生成されたとき
+     */
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 
+    /**
+     * ボタンがクリックされたとき
+     */
 	public void onClick(View v) {
-		// TODO 自動生成されたメソッド・スタブ
+		/* パズルをシャッフルする */
 		puzzle.shuffle();
+	}
+
+	/**
+	 * バナーがタッチされたとき
+	 */
+	public boolean onTouch(View v, MotionEvent event) {
+
+		return false;
 	}
 }
--- src/bemax/puzzle/Panel.java
+++ src/bemax/puzzle/Panel.java
@@ -6,18 +6,29 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 
+/**
+ * パズルに組み込まれる一つひとつのパネルクラス
+ * @author horikawa
+ */
 class Panel {
 	private int num;			// パネル番号
 	private Bitmap image;
 	private final Rect rect;
 	private boolean move;
 
-	public Panel(int n, Bitmap img){
-		num = n;
+	/**
+	 * コンストラクタ
+	 * @param number パネル番号
+	 * @param img 画像データ
+	 */
+	public Panel(int number, Bitmap img){
+		/* インスタンス変数初期化 */
+		num = number;
 		image = img;
 		rect = new Rect(0, 0, img.getWidth(), img.getHeight());
 		move = false;
 
+		/* パネル画像に黒の枠線を描く */
 		Canvas canvas = new Canvas(image);
 		Paint paint = new Paint();
 		paint.setColor(Color.BLACK);
@@ -25,18 +36,34 @@ class Panel {
 		canvas.drawRect(rect.left, rect.top, rect.right-1, rect.bottom-1, paint);
 	}
 
+	/**
+	 * インスタンスの画像データを返す
+	 * @return 画像データ
+	 */
 	Bitmap getImage(){
 		return image;
 	}
 
+	/**
+	 * インスタンスの移動中情報をセットする
+	 * @param b 移動状態ならtrue
+	 */
 	void setMove(boolean b){
 		move= b;
 	}
 
+	/**
+	 * インスタンスの移動状態を返す
+	 * @return 移動状態ならtrue
+	 */
 	boolean getMove(){
 		return move;
 	}
 
+	/**
+	 * インスタンスのパネル番号を返す
+	 * @return パネル番号
+	 */
 	int getNum(){
 		return num;
 	}
