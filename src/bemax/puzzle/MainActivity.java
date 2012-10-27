df5048919b2f2b0418c92d76404413504c029c0c
masaaki horikawa <horikawa@bemax.jp>
2012-10-27 09:25:55 +0900

    menuアイテム追加

--- src/bemax/puzzle/MainActivity.java
+++ src/bemax/puzzle/MainActivity.java
@@ -2,36 +2,65 @@ package bemax.puzzle;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener, OnMenuItemClickListener{
 	private SurfaceView puzView;
 	private Button button;
 	private Puzzle puzzle;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        /* ここから入力 */
-        puzView = (SurfaceView)findViewById(R.id.puzzle_view);
-        button = (Button)findViewById(R.id.reset_button);
-        puzzle = new Puzzle(puzView);
-        button.setOnClickListener(this);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		/* パズル用ビューをレイアウトから取得 */
+		puzView = (SurfaceView)findViewById(R.id.puzzle_view);
+
+		/* シャッフルボタンをレイアウトから取得 */
+		button = (Button)findViewById(R.id.reset_button);
+
+		/* パズルオブジェクトを初期化 */
+		puzzle = new Puzzle(puzView);
+
+		/* シャッフルボタンにリスナーを追加 */
+		button.setOnClickListener(this);
+	}
 
 	public void onClick(View v) {
-		// TODO 自動生成されたメソッド・スタブ
 		puzzle.shuffle();
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	public boolean onMenuItemClick(MenuItem item) {
+		int id = item.getItemId();
+		switch(id){
+			/* ギャラリーメニューがタッチされた時 */	
+			case R.id.gallery_menu:
+				
+				
+				break;
+				
+			/* ビーマックスメニューがタッチされた時 */
+			case R.id.bemax_menu:
+				Intent intent = new Intent();
+				
+				
+				break;
+				
+			default:
+		}
+		return false;
 	}
 }
